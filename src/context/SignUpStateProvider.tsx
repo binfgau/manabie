dab0e7cfeda2c6e7ffeb4d52ae8b7a23d42ac10c
Nguyen Van Binh <binfgau>
2022-06-21 10:49:34 +0700

    feature: create Sign Up State Provider to provide state of stepper  states

---
+++ src/context/SignUpStateProvider.tsx
@@ -0,0 +1,45 @@
+import { Dispatch, useMemo, createContext, useContext, ReactNode } from 'react';
+import {
+  SignUpAction,
+  SignUpState,
+  ContextValue,
+  SignUpStateReturn,
+} from '../pages/signup/multi-step-form-state/types';
+
+export const SignUpContext = createContext<ContextValue>(undefined);
+
+interface SignUpStateProviderProps {
+  signUpState: SignUpState;
+  signUpDispatch: Dispatch<SignUpAction>;
+  children: ReactNode;
+}
+
+export const SignUpStateProvider = ({
+  signUpState,
+  signUpDispatch,
+  children,
+}: SignUpStateProviderProps): JSX.Element => {
+  const value: [SignUpState, Dispatch<SignUpAction>] = useMemo(
+    () => [signUpState, signUpDispatch],
+    [signUpState, signUpDispatch]
+  );
+  return (
+    <SignUpContext.Provider value={value}>{children}</SignUpContext.Provider>
+  );
+};
+
+export const useSignUpState = (): SignUpStateReturn => {
+  const signUpContext = useContext(SignUpContext);
+  if (!signUpContext) {
+    throw new Error(`useSignUpState must be used within a SignUpStateProvider`);
+  }
+
+  const [signUpState, signUpDispatch] = signUpContext;
+
+  const { signUpData } = signUpState;
+
+  return {
+    signUpData,
+    signUpDispatch,
+  };
+};
