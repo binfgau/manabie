e42f91607c7c520a7ef909d71125ddf598565c91
Nguyen Van Binh <binfgau>
2022-06-21 10:55:29 +0700

    feature: using Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import { Container, CssBaseline } from '@mui/material';
+import React, { useReducer } from 'react';
+import { CssBaseline } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import SignUp from './features/signup/SignUp';
+import SignUp from './pages/signup/SignUp';
 import Layout from './layout/Layout';
+import { SignUpState } from './pages/signup/multi-step-form-state/types';
+import { signUpReducer } from './pages/signup/multi-step-form-state/reducer';
+import { SignUpStateProvider } from './context/SignUpStateProvider';
 
 const theme = createTheme();
 
-const App: React.FC = () => {
+const App = (): JSX.Element => {
+  const defaultState: SignUpState = {
+    signUpData: {
+      formConfig: {
+        activeStep: 0,
+        steps: [
+          { id: 0, name: 'User Information', isCompleted: false },
+          { id: 1, name: 'Upload Avatar', isCompleted: false },
+          { id: 2, name: 'Review Profile', isCompleted: false },
+        ],
+      },
+    },
+  };
+
+  const [signUpState, signUpDispatch] = useReducer<typeof signUpReducer>(
+    signUpReducer,
+    defaultState
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Switch>
-          <Route exact path='/' component={SignUp} />
-          <Route path='*'>
-            <Redirect to='/' />
-          </Route>
-        </Switch>
-      </Layout>
+      <SignUpStateProvider
+        signUpState={signUpState}
+        signUpDispatch={signUpDispatch}
+      >
+        <CssBaseline />
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={SignUp} />
+            <Route path='*'>
+              <Redirect to='/' />
+            </Route>
+          </Switch>
+        </Layout>
+      </SignUpStateProvider>
     </ThemeProvider>
   );
 };
