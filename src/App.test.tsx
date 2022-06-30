795d4ac1f0066f9b87f0cddb93aeb6e7823a90ed
Nguyen Van Binh <binfgau>
2022-06-30 14:51:21 +0700

    feature: final unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,51 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
-test('sign up for happy path', () => {
+test('sign up for happy path', async () => {
   render(<App />);
+
+  // Step Personal Info
+
+  const firstNameTextField = screen.getByRole('textbox', {
+    name: 'First name',
+  });
+  expect(firstNameTextField).toBeInTheDocument();
+
+  const lastNameTextField = screen.getByRole('textbox', {
+    name: 'Last name',
+  });
+  expect(lastNameTextField).toBeInTheDocument();
+
+  userEvent.type(firstNameTextField, 'Van');
+  userEvent.type(lastNameTextField, 'Binh');
+
+  const buttonNextAtPersonal = screen.getByRole('button', { name: 'Next' });
+  expect(buttonNextAtPersonal).toBeInTheDocument();
+
+  await act(() => {
+    userEvent.click(buttonNextAtPersonal);
+  });
+
+  // Step Avatar Info
+
+  const buttonUploadAvatar = screen.getByRole('button', {
+    name: 'Upload',
+  });
+  expect(buttonUploadAvatar).toBeInTheDocument();
+
+  userEvent.click(buttonUploadAvatar);
+
+  const buttonNextAtAvatar = screen.getByRole('button', { name: 'Next' });
+  expect(buttonNextAtAvatar).toBeInTheDocument();
+
+  await act(() => {
+    userEvent.click(buttonNextAtAvatar);
+  });
+
+  // Step Review Info
+
+  const fullName = screen.getByText('Van Binh');
+  expect(fullName).toBeInTheDocument();
 });
